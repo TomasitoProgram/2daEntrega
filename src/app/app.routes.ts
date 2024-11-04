ef4179f7fcfaf0c4abd6f39be0755e5c96726bc5
Macarena <mac.lobos@duocuc.cl>
2024-11-04 12:17:36 -0300

    agregando ando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -25,4 +25,25 @@ export const routes: Routes = [
     path: 'home',
     loadComponent: () => import('./pages/home/home.page').then( m => m.HomePage)
   },
+  {
+    path: 'correo',
+    loadComponent: () => import('./pages/correo/correo.page').then( m => m.CorreoPage)
+  },  {
+    path: 'pregunta',
+    loadComponent: () => import('./pages/pregunta/pregunta.page').then( m => m.PreguntaPage)
+  },
+  {
+    path: 'correcto',
+    loadComponent: () => import('./pages/correcto/correcto.page').then( m => m.CorrectoPage)
+  },
+  {
+    path: 'incorrecto',
+    loadComponent: () => import('./pages/incorrecto/incorrecto.page').then( m => m.IncorrectoPage)
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+
+
 ];
---
+++ src/app/pages/pregunta/pregunta.page.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonInput, IonButton, IonItem, IonLabel, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { User } from 'src/app/model/user';
+import { ActivatedRoute, NavigationExtras, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-pregunta',
+  templateUrl: './pregunta.page.html',
+  styleUrls: ['./pregunta.page.scss'],
+  standalone: true,
+  imports: [IonContent,IonInput, IonButton, IonLabel, IonItem, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+})
+export class PreguntaPage {
+
+  public usuario = new User();
+  public respuestaSecreta: string;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute) {
+    this.usuario = new User();
+    this.respuestaSecreta = '';
+    this.activatedRoute.queryParams.subscribe(() => {
+      const nav = router.getCurrentNavigation();
+      if (nav) {
+        if (nav.extras.state) {
+          this.usuario = nav.extras.state['user'];
+        }
+      }
+    });
+  }
+
+  recuperarContrasena() {
+    // Lógica para validar la respuesta y recuperar la contraseña
+
+    if (this.respuestaSecreta === this.usuario.secretAnswer) {
+      const navigationExtras: NavigationExtras = {
+        state: {
+          usuario: this.usuario,
+        }
+      }
+      this.router.navigate(['/correcto'], navigationExtras);
+    } else {
+      this.router.navigate(['/incorrecto']);
+    }
+  }
+
+  iniciarSesion() {
+    this.router.navigate(['/login']);
+  }
+
+
+}
